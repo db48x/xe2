21a629e5a2867faa6fd995b04d454f1607a1628a
David O'Toole <dto@gnu.org>
2009-01-23 01:02:48 -0500

    testing

--- pool.lisp
+++ pool.lisp
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2009  David O'Toole
 
 ;; Author: David O'Toole <dto@gnu.org>
-;; Keywords: lisp, data
+;; Keywords: lisp, data, tools
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
