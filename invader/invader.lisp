0e2788752efc647eb2e8c88f134dead76c7945bb
David O'Toole <dto@gnu.org>
2009-03-24 00:09:02 -0400

    put pallets all the way to right and bottom walls

--- invader/invader.lisp
+++ invader/invader.lisp
@@ -673,8 +673,8 @@
   (attacking-with :initform :robotic-arm)
   (energy :initform (make-stat :base 800 :min 0 :max 1000))
   (firing-with :initform :robotic-arm)
-  (strength :initform (make-stat :base 13))
-  (dexterity :initform (make-stat :base 9)))
+  (strength :initform (make-stat :base 16))
+  (dexterity :initform (make-stat :base 12)))
 
 (define-method choose-new-direction scanner ()
   (setf <direction>
@@ -732,8 +732,8 @@
       (trace-rectangle #'drop-wall
 		       0 0 width height)
       ;; drop pallets
-      (let ((imax (1- (truncate (/ width pallet-size))))
-	    (jmax (1- (truncate (/ height pallet-size)))))
+      (let ((imax (truncate (/ width pallet-size)))
+	    (jmax (truncate (/ height pallet-size))))
 	(dotimes (i imax)
 	  (dotimes (j jmax)
 	    ;; don't wall in the player
