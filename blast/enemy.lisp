324b2c2bcec3ba3d5cdb86c868c6f5b2c5ecd987
David O'Toole <dto@gnu.org>
2009-07-14 11:12:48 -0400

    nebula added

--- blast/enemy.lisp
+++ blast/enemy.lisp
@@ -166,6 +166,52 @@
   [say *billboard* :destroy]
   [parent>>die self])
 
+;;; The Canaz ship
+
+(defcell canaz 
+  (tile :initform "canaz")
+  (hit-points :initform (make-stat :base 4 :max 4 :min 0))
+  (speed :initform (make-stat :base 4))
+  (strength :initform (make-stat :base 10))
+  (defense :initform (make-stat :base 10))
+  (energy :initform (make-stat :base 100 :min 0 :max 100))
+  (stepping :initform t)
+  (movement-cost :initform (make-stat :base 10))
+  (equipment-slots :initform '(:center-bay))
+  (firing-with :initform :center-bay)
+  (max-items :initform (make-stat :base 2))
+  (direction :initform (random-direction))
+  (movement-cost :initform (make-stat :base 10))
+  (categories :initform '(:actor :obstacle :enemy :target)))
+
+(define-method run canaz ()
+  (clon:with-field-values (row column) self
+    (let* ((world *active-world*)
+	   (direction [direction-to-player *active-world* row column])
+	   (distance [distance-to-player *active-world* row column]))
+      (if (< distance 8)
+	  (progn 
+	    (setf <direction> (if (< distance 4)
+				  (random-direction)
+				  direction))
+	    [>>fire self direction])
+	  ;; bounce around 
+	  (progn 
+	    (when [obstacle-in-direction-p *active-world* <row> <column> <direction>]
+	      (setf <direction> (random-direction)))
+	    [>>move self <direction>])))))
+
+(define-method die canaz ()
+  [play-sample self "death-alien"]
+  [drop self (clone =energy=)]
+  [say *billboard* :destroy]
+  [parent>>die self])
+
+(define-method loadout canaz ()
+  [make-inventory self]
+  [make-equipment self]
+  [equip self [add-item self (clone =muon-cannon=)]])
+
 ;;; The Berserker is a relatively simple AI enemy.
 
 ;; Run in a straight line until hitting an obstacle.
---
+++ blast/nebula.lisp
@@ -0,0 +1,131 @@
+
+(in-package :blast)
+
+;;; The evil boss stations must be destroyed.
+
+(defcell station-arm-horz 
+  (tile :initform "station-arm-horz")
+  (categories :initform '(:obstacle :opaque :destructible :target))
+  (hit-points :initform (make-stat :base 10 :min 0)))
+
+(defcell station-arm-vert 
+  (tile :initform "station-arm-vert")
+  (categories :initform '(:obstacle :opaque :destructible :target))
+  (hit-points :initform (make-stat :base 10 :min 0)))
+
+(defcell station-base 
+  (tile :initform "station-base")
+  (categories :initform '(:obstacle :actor :equipper :opaque))
+  (speed :initform (make-stat :base 6))
+  (hit-points :initform (make-stat :base 40 :min 0))
+  (equipment-slots :initform '(:robotic-arm))
+  (max-items :initform (make-stat :base 3))
+  (stepping :initform t)
+  (attacking-with :initform :robotic-arm)
+  (energy :initform (make-stat :base 800 :min 0 :max 1000))
+  (firing-with :initform :robotic-arm)
+  (strength :initform (make-stat :base 13))
+  (dexterity :initform (make-stat :base 9)))
+
+(defvar *station-base-count* 0)
+
+(define-method loadout station-base ()
+  (let ((cannon (clone =lepton-cannon=)))
+    [equip self [add-item self cannon]])
+  (incf *station-base-count*))
+
+(define-method initialize station-base ()
+  [make-inventory self]
+  [make-equipment self])
+
+(define-method run station-base ()
+  (clon:with-field-values (row column) self
+    (let ((world *active-world*))
+      (if (< [distance-to-player world row column] 10)
+	  (let ((player-dir [direction-to-player world row column]))
+	    [expend-default-action-points self]
+	    [queue>>fire self player-dir])))))
+
+(define-method die station-base ()
+  (decf *station-base-count*)
+  (when (= 0 *station-base-count*)
+    [>>narrateln :narrator "YOU WIN!"])
+  [parent>>die self])
+
+(defun paint-station-piece (world row column maxsize)
+  (when (not [obstacle-at-p world row column])
+    (labels ((drop-horz (r c)
+	       [drop-cell world (clone =station-arm-horz=) r c])
+	     (drop-vert (r c)
+	       [drop-cell world (clone =station-arm-vert=) r c])
+	     (drop-base (r c)
+	       [drop-cell world (clone =station-base=) r c :loadout t]))
+      (trace-row #'drop-horz row column (max 0 (- column (random maxsize))))
+      (trace-row #'drop-horz row column (+ column (random maxsize)))
+      (trace-column #'drop-vert column row (max 0 (- row (random maxsize))))
+      (trace-column #'drop-vert column row (+ row (random maxsize)))
+      (drop-base row column))))
+
+(defcell nebula-space 
+  (tile :initform "nebula"))
+
+(defcell red-plasma
+  (tile :initform "red-plasma"))
+
+(defcell charged-nebula-space
+  (tile :initform "nebula2"))
+
+(defcell protostar
+  (tile :initform "protostar"))
+
+(define-method step protostar (stepper)
+  (when [is-player stepper]
+    [>>say :narrator "You ram into the white-hot photostar and vaporize."]
+    [>>die stepper]))
+
+(define-prototype nebula-m (:parent rlx:=world=)
+  (name :initform "Restricted Nebula M")
+  (ambient-light :initform :total))
+  
+(define-method drop-plasma nebula-m ()
+  (clon:with-field-values (height width) self
+    (let ((plasma (rlx:render-plasma height width :graininess 0.7))
+	  (value nil))
+      (dotimes (i (- height 10))
+	(dotimes (j (- width 10))
+	  (setf value (aref plasma i j))
+	  (when (< 0 value)
+	    (let ((object =red-plasma=))
+	      [drop-cell self (clone object) (+ 10 i) (+ 10 j) :no-collisions t])))))))
+
+(define-method generate nebula-m (&key (height 100)
+				       (width 100)
+				       (protostars 70)
+				       (energy 30)
+				       (rooks 30)
+				       (canaz 60))
+  (setf <height> height <width> width)
+  [create-default-grid self]
+  (dotimes (i width)
+    (dotimes (j height)
+      [drop-cell self (clone (if (zerop (random 7))
+				 =charged-nebula-space= 
+				 =nebula-space=))
+		 i j]))
+  [drop-plasma self]
+  (dotimes (i rooks)
+    [drop-cell self (clone =rook=) (random height) (random width) :loadout t])
+  (dotimes (i energy)
+    [drop-cell self (clone =energy=) (random height) (random width) :loadout t])
+  (dotimes (i protostars)
+    [drop-cell self (clone =protostar=) (random height) (random width)])
+  (dotimes (i canaz)
+    [drop-cell self (clone =canaz=) (random height) (random width) :loadout t])
+  (setf *station-base-count* 0)
+  (loop do (paint-station-piece self (random height) (random width) 10)
+	while (< *station-base-count* 20)))
+
+  
+(define-method start nebula-m ()
+  (play-music "black-thunder" :loop t)
+  [parent>>start self])
