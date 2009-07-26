d1c4782e24b0db91586db5dc856b2f63e59deaae
David O'Toole <dto@gnu.org>
2009-07-26 15:36:37 -0400

    mars ice caps

--- blast/mars.lisp
+++ blast/mars.lisp
@@ -11,6 +11,9 @@
 (defcell mars-icy2
   (tile :initform "mars-terrain-icy2"))
 
+(defcell mars-tundra 
+  (tile :initform "mars-tundra"))
+
 (defcell mars-dark 
   (tile :initform "mars-terrain-dark"))
 
@@ -33,15 +36,25 @@
 	(dotimes (j width)
 	  (setf value (aref dust-plasma i j))
 	  [drop-cell self (clone (if (< 0 value)
-				     (if (< -0.3 value) 
+				     (if (< 0.3 value) 
 					 =mars-flat=
 					 =mars-cracks=)
 				     =mars-dark=))
 		     i j :no-collisions t]
 	  (setf value (aref ice-plasma i j))
 	  (when (< 0 value)
-	    [drop-cell self (clone =mars-icy2=) i j]))))))
+	    [drop-cell self (clone =mars-icy2=) i j])))
+      ;; draw polar ice caps
+      (dotimes (j width)
+	(dotimes (n (1+ (random 3)))
+	  [drop-cell self (clone =mars-tundra=) n j]
+	  [drop-cell self (clone =mars-tundra=) (- height n) j])))))
+	
 
 (define-method generate mars (&rest parameters)
   [create-default-grid self]
   [draw-terrain self])
+
+(define-method start mars ()
+  (play-music "black-thunder" :loop t)
+  [parent>>start self])
